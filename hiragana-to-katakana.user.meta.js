12e2fa7a71c666f2cea69391baf88b375a839593
Dobromir Stefanov <dobromir.stefanov@gmail.com>
2014-08-22 16:38:02 +0100

    Add author to metadata

--- hiragana-to-katakana.user.meta.js
+++ hiragana-to-katakana.user.meta.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name        Hiragana to Katakana
 // @description Convert all hiragana on every site to the equivalent katakana
+// @author      Dobromir Stefanov
 // @namespace   http://code.lfire.net/user-scripts
 // @downloadURL http://code.lfire.net/user-scripts/hiragana-to-katakana.user.js
 // @include     *
