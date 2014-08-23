770f8dfa04e02a4f905e33e4f60f5d9cdd2554dd
Dobromir Stefanov <dobromir.stefanov@gmail.com>
2014-08-23 14:20:11 +0100

    Add @updateURL to metadata

--- hiragana-to-katakana.user.meta.js
+++ hiragana-to-katakana.user.meta.js
@@ -4,6 +4,7 @@
 // @author      Dobromir Stefanov
 // @namespace   http://code.lfire.net/user-scripts
 // @downloadURL http://code.lfire.net/user-scripts/hiragana-to-katakana.user.js
+// @updateURL   http://code.lfire.net/user-scripts/hiragana-to-katakana.user.meta.js
 // @include     *
-// @version     1.0
+// @version     1.1
 // ==/UserScript==
