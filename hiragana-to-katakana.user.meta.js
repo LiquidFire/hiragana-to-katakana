2cfbefbeaa511e7134ce5c56d24b8f6d448b66dc
Dobromir Stefanov <dobromir.stefanov@gmail.com>
2014-08-23 15:54:54 +0100

    Switch to https URL

--- hiragana-to-katakana.user.meta.js
+++ hiragana-to-katakana.user.meta.js
@@ -2,9 +2,9 @@
 // @name        Hiragana to Katakana
 // @description Convert all hiragana on every site to the equivalent katakana
 // @author      Dobromir Stefanov
-// @namespace   http://code.lfire.net/user-scripts
-// @downloadURL http://code.lfire.net/user-scripts/hiragana-to-katakana.user.js
-// @updateURL   http://code.lfire.net/user-scripts/hiragana-to-katakana.user.meta.js
+// @namespace   https://lfire.net/user-scripts
+// @downloadURL https://lfire.net/user-scripts/hiragana-to-katakana.user.js
+// @updateURL   https://lfire.net/user-scripts/hiragana-to-katakana.user.meta.js
 // @include     *
-// @version     1.1
+// @version     1.2
 // ==/UserScript==
